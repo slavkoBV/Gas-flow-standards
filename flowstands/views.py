d0f43f9d47cfe2dae5336efeeebc972a6b4217aa
slavkoBV <slav_b@ukr.net>
2016-08-25 16:39:36 +0300

    modify custom digg-style paginator

--- flowstands/views.py
+++ flowstands/views.py
@@ -44,38 +44,41 @@ def flowstands_list(request):
 	
 	# digg-paginator realization
 	page_range = []
-	ON_EACH_SIDE = 3
-    ON_ENDS = 2
+	ON_EACH_SIDE = 2
+	ON_ENDS = 3
 	DOT = '...'
+	page_num = flowstands.number
+	page_range = []
 	
 	# If there are 10 or fewer pages, display links to every page.
     # Otherwise, do some fancy
-    if paginator.num_pages <= 10:
-        page_range = range(paginator.num_pages)
-    else:
+	if paginator.num_pages <= 5:
+		page_range = range(paginator.num_pages)
+	else:
         # Insert "smart" pagination links, so that there are always ON_ENDS
         # links at either end of the list of pages, and there are always
         # ON_EACH_SIDE links at either end of the "current page" link.
-         page_range = []
-        if page_num > (ON_EACH_SIDE + ON_ENDS):
-			page_range.extend(range(0, ON_ENDS))
+		page_range = []
+		if page_num > (ON_EACH_SIDE + ON_ENDS):
+			page_range.extend(range(1, ON_ENDS))
 			page_range.append(DOT)
-            page_range.extend(range(page_num - ON_EACH_SIDE, page_num + 1))
-        else:
-            page_range.extend(range(0, page_num + 1))
-        if page_num < (paginator.num_pages - ON_EACH_SIDE - ON_ENDS - 1):
-            page_range.extend(range(page_num + 1, page_num + ON_EACH_SIDE + 1))
-            page_range.append(DOT)
-            page_range.extend(range(paginator.num_pages - ON_ENDS, paginator.num_pages))
-        else:
-            page_range.extend(range(page_num + 1, paginator.num_pages))
+			page_range.extend(range(page_num - ON_EACH_SIDE, page_num + 1))
+		else:
+			page_range.extend(range(1, page_num + 1))
+		if page_num < (paginator.num_pages - ON_EACH_SIDE - ON_ENDS - 1):
+			page_range.extend(range(page_num + 1, page_num + ON_EACH_SIDE + 1))
+			page_range.append(DOT)
+			page_range.extend(range(paginator.num_pages - ON_ENDS, paginator.num_pages+1))
+		else:
+			page_range.extend(range(page_num + 1, paginator.num_pages+1))
 	
 	return render(request, 'flowstands/flowstands_list.html', {
 		'flowstands': flowstands,
 		'q': q,
 		'count': count,
 		'today': today,
-		'page_range': page_range
+		'page_range': page_range,
+		'DOT': DOT
 		}
 	)
 ########################################################################
